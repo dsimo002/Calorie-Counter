f1b3f021cc98700f4dd772edb59e9b73514fb7bd
oakami <clee101@ucr.edu>
2016-10-22 03:17:33 -0700

    Update LoginActivity.java

--- app/src/main/java/com/cs180/team2/caloriecounter/LoginActivity.java
+++ app/src/main/java/com/cs180/team2/caloriecounter/LoginActivity.java
@@ -211,12 +211,10 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email != "";
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return password.length() > 0;
     }
 
