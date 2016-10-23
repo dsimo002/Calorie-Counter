8c7c8a25d4eeb0527d9d99c92b3b1cf9ad86bd3f
Clayton Su <csu007@ucr.edu>
2016-10-23 03:44:27 -0700

    Added forgot username/password

---
+++ app/src/main/java/com/cs180/team2/caloriecounter/ForgotPasswordActivity.java
@@ -0,0 +1,43 @@
+package com.cs180.team2.caloriecounter;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ForgotPasswordActivity.this); //Chanho: Dialogs are popup notifications that require users to interact with to get rid of.
+        builder.setMessage("Did you forget your username or your password?"); //Asks for user name or password
+            //THIS IS GAY DONT USE THIS ACTIVITY
+
+        builder.setPositiveButton("Username", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        builder.setNegativeButton("Password", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
+    }
+
+}
