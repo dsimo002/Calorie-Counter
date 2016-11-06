90e42227cf8774bcc519fe39aea48a7df9a9378a
brian921 <cerophire123@gmail.com>
2016-11-05 19:45:08 -0700

    Slight Changes to GUI and Guest user restrictions
    Changed app so guest cannot add custom foods or change passwords. Also
    slightly changed Daily Calories activity GUI.

--- app/src/main/java/com/cs180/team2/caloriecounter/AddEntrySnacks.java
+++ app/src/main/java/com/cs180/team2/caloriecounter/AddEntrySnacks.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -32,6 +33,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import static com.cs180.team2.caloriecounter.LoginActivity.username;
+import static com.cs180.team2.caloriecounter.R.id.addcustomfoodbutton;
 import static com.cs180.team2.caloriecounter.R.id.textView2;
 
 
@@ -74,6 +77,14 @@ public class AddEntrySnacks extends AppCompatActivity {
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        Button customFood = (Button) findViewById(addcustomfoodbutton);
+        if(username.isEmpty())   //if guest user, don't allow them to add custom food
+        {
+            customFood.setVisibility(View.INVISIBLE);
+        }
+        else
+            customFood.setVisibility(View.VISIBLE);
     }
 
 
